7ef4c8a9fb129bb93b1f01390d28a2a0573d534d
jacobitkashala <jacobitkashala@gmail.com>
2021-09-29 11:11:06 +0200

    responsive form2

--- final/src/components/FomPerson.js
+++ final/src/components/FomPerson.js
@@ -10,6 +10,9 @@ const FormPerson = ({ handlerClickPrevForm }) => {
 	const listPersons = useSelector((state) => (state.persons))
 	const dispatch = useDispatch();
 
+	const handlerClickDeletePerson = (id) => {
+		console.log(id);
+	}
 	const onSubmit = (event) => {
 		event.preventDefault();
 		dispatch(addPerson({
@@ -26,94 +29,155 @@ const FormPerson = ({ handlerClickPrevForm }) => {
 	};
 
 	return (
-		<div className='row'>
-			<form onSubmit={onSubmit} className='mt-3 mb-3 col-md-4 col-sm-12'>
+		<>
+			<div className='row mt-3'>
+				<div className='col-sm-8'>
+					<h3>Local</h3>
+					<table className="table col-sm-10 w-25 table-dark table-hover table-striped">
+						<thead>
+							<tr>
+								<th scope="col">#</th>
+								<th scope="col">Nom</th>
+								<th scope="col">Prénom</th>
+								<th scope="col">Companie</th>
+								<th scope="col">Grade</th>
+								<th scope="col">Supprimer</th>
+								<th scope="col">Modifier</th>
+							</tr>
+						</thead>
+						<tbody>
+							{
+								[...listPersons, ...listCompanies].map((element, index) => {
+									// console.log(companie);
+									return (
+										<tr key={index} className="datacompanie">
+											<th scope="row">{index + 1}</th>
 
-				<label>Nom</label>
-				<input
-					type='text'
-					className='form-control mb-2 mr-sm-2 col-sm-12'
-					placeholder='Nom ...'
-					value={nom}
-					onChange={(event) => setNom(event.target.value)}
-				/>
-				<label >Prénom</label>
-				<input
-					type='text'
-					className='form-control mb-2 mr-sm-2 col-sm-12'
-					placeholder='Prénom ...'
-					value={prénom}
-					onChange={(event) => setPrénom(event.target.value)}
-				/>
-				<button type='button' className='btn btn-primary mb-2' onClick={handlerClickPrevForm}>
-					Preced
-				</button>
-				&nbsp;&nbsp;
-				<button type='submit' className='btn btn-primary mb-2'>
-					Enregis
-				</button>
-			</form>
-			<div className='mt-3 mb-3 col-sm-12 col-md-4'>
-				<table className="table col-10 w-100 table-dark table-hover table-striped" style={{cursor:'point'}}>
-					<thead>
-						<tr>
-							<th scope="col">#</th>
-							<th scope="col">Nom</th>
-							<th scope="col">Prénom</th>
-							<th scope="col">Companie</th>
-							<th scope="col">Grade</th>
-						</tr>
-					</thead>
-					<tbody>
-						{[...listCompanies, ...listPersons].map((element, index) => {
-							console.log(element);
-							// if(){
 
-							// }
-							return (
-								<tr key={index} className="datacompanie">
-									<th scope="row">{index + 1}</th>
-									<td>{element.nom}</td>
-									<td>{element.prenom}</td>
-									<td>{element.companie}</td>
-									<td>{element.grade}</td>
-								</tr>
-							)
-						}
-						)
-						}
-					</tbody>
-				</table>
+											<td>{element.nom}</td>
+											<td>{element.prenom}</td>
+											<td>{element.companie}</td>
+											<td>{element.grade}</td>
+											<td>
+												<button type='button' className='btn btn-danger m-1' onClick={() => { handlerClickDeletePerson(element.id) }}  >
+													supprimer
+												</button>
+											</td>
+											<td>
+												<button type='button' className='btn btn-primary m-1'  >
+													modifier
+												</button>
+											</td>
+										</tr>
+									)
+								})
+							}
+						</tbody>
+					</table>
+				</div>
+				<form onSubmit={onSubmit} className='col-sm-12 col-md-4 col-lg-4'>
+					<h3>Information</h3>
+					<input
+						type='text'
+						className='form-control mb-2 mr-sm-2 col-sm-12'
+						placeholder='Nom ...'
+						value={nom}
+						onChange={(event) => setNom(event.target.value)}
+					/>
+					<label >Prénom</label>
+					<input
+						type='text'
+						className='form-control mb-2 mr-sm-2 col-sm-12'
+						placeholder='Prénom ...'
+						value={prénom}
+						onChange={(event) => setPrénom(event.target.value)}
+					/>
+
+					<button type='submit' className='btn btn-success mb-2 mg-10'>
+						Enre
+					</button>
+					&nbsp;&nbsp;
+
+					<button type='button' className='btn btn-primary mb-2  mg-10' onClick={handlerClickPrevForm} >
+						Prec
+					</button>
+				</form>
 			</div>
-			<div className='mt-3 m-1 col-sm-12 col-md-4'>
-				<table className="table col-10 w-100 table-dark table-hover table-striped" style={{cursor:'point'}}>
-					<thead>
-						<tr>
-							<th scope="col">#</th>
-							<th scope="col">Nom</th>
-							<th scope="col">Prénom</th>
-							<th scope="col">Companie</th>
-							<th scope="col">Grade</th>
-						</tr>
-					</thead>
-					<tbody>
-						{[...listCompanies, ...listPersons].map((element, index) => {
-							return (
-								<tr key={index} className="datacompanie">
-									<th scope="row">{index + 1}</th>
-									<td>{element.nom}</td>
-									<td>{element.prenom}</td>
-									<td>{element.companie}</td>
-									<td>{element.grade}</td>
-								</tr>
-							)
-						}
-						)
-						}
-					</tbody>
-				</table>
+			<div className='row mt-3'>
+				<div className='col-sm-8'>
+					<h3>Local</h3>
+					<table className="table col-sm-10 w-25 table-dark table-hover table-striped">
+						<thead>
+							<tr>
+								<th scope="col">#</th>
+								<th scope="col">Nom</th>
+								<th scope="col">Prénom</th>
+								<th scope="col">Companie</th>
+								<th scope="col">Grade</th>
+								<th scope="col">Supprimer</th>
+								<th scope="col">Modifier</th>
+							</tr>
+						</thead>
+						<tbody>
+							{
+								[...listPersons, ...listCompanies].map((element, index) => {
+									console.log(element);
+									return (
+										<tr key={index} className="datacompanie">
+											<th scope="row">{index + 1}</th>
+
+
+											<td>{element.nom}</td>
+											<td>{element.prenom}</td>
+											<td>{element.companie}</td>
+											<td>{element.idCompani}</td>
+											<td>
+												<button type='button' className='btn btn-danger m-1' onClick={() => { handlerClickDeletePerson(element.id) }}  >
+													supprimer
+												</button>
+											</td>
+											<td>
+												<button type='button' className='btn btn-primary m-1'  >
+													modifier
+												</button>
+											</td>
+										</tr>
+									)
+								})
+							}
+						</tbody>
+					</table>
+				</div>
+				<form onSubmit={onSubmit} className='col-sm-12 col-md-4 col-lg-4'>
+					<h3>Information</h3>
+					<input
+						type='text'
+						className='form-control mb-2 mr-sm-2 col-sm-12'
+						placeholder='Nom ...'
+						value={nom}
+						onChange={(event) => setNom(event.target.value)}
+					/>
+					<label >Prénom</label>
+					<input
+						type='text'
+						className='form-control mb-2 mr-sm-2 col-sm-12'
+						placeholder='Prénom ...'
+						value={prénom}
+						onChange={(event) => setPrénom(event.target.value)}
+					/>
+
+					<button type='submit' className='btn btn-success mb-2 mg-10'>
+						Enre
+					</button>
+					&nbsp;&nbsp;
+
+					<button type='button' className='btn btn-primary mb-2  mg-10' onClick={handlerClickPrevForm} >
+						Prec
+					</button>
+				</form>
 			</div>
-		</div>
+		</>
+
 	);
 };
 
